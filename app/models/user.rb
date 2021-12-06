996ec25d69631a162793bc16dc73c55f5bdc94e6
Reshy <reshy@protonmail.com>
2021-12-06 18:58:42 +0100

    Add views for index and user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   # A method that returns the 3 most recent posts for a given user.
 
   def show_posts(id)
-    posts.where('user_id = ?', id).order(created_at: :asc).limit(3)
+    Post.where('user_id = ?', id).order(created_at: :asc).limit(3)
   end
 end
