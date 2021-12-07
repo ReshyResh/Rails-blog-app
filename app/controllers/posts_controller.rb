b57b21375846f45fa9c129b32d7f9ab2afec9d89
Reshy <reshy@protonmail.com>
2021-12-07 16:41:39 +0100

    Add form to create post, add validation for user id and post id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,34 @@ class PostsController < ApplicationController
 
   def show
     return unless Post.exists?(id: params['id'])
-
     @post = Post.find_by(id: params['id'])
-    render '../views/layouts/partials/_userpostdetails'
+    if (@post.user_id == params['user_id'].to_i)
+      render '../views/layouts/partials/_userpostdetails'
+    else
+      render '../views/layouts/partials/_wrongid'
+    end
+  end
+
+  def new
+    @newpost = Post.new
+    respond_to do |format|
+      format.html { render :new }
+    end
+  end
+  
+  def create
+    # newpost = Post.new(Time.now, Time.now, 1, title, text, 0, 0)
+    newpost = Post.new(params.require(:post).permit(:title, :text).merge(created_at: Time.now, updated_at: Time.now,user_id: 2, comments_counter: 0, likes_counter: 0))
+    respond_to do |format|
+      format.html do
+        if newpost.save
+          flash[:success] = "Post created successfully"
+          redirect_to user_posts_path
+        else 
+          flash.now[:error] = "Error: Post could not be created"
+          render :new, locals: { newpost: newpost }
+        end
+      end
+    end
   end
 end
